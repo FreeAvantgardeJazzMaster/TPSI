aad7dec625d0c448063393be406b7e2e372c2aab
Adam <a.kokosza.95@gmail.com>
2018-03-21 13:54:42 +0100

    Dodanie linkow

--- src/tpsi_server.java
+++ src/tpsi_server.java
@@ -3,8 +3,12 @@ import com.sun.net.httpserver.*;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URLConnection;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.file.Files.probeContentType;
 
 public class tpsi_server {
     public static void main(String[] args) throws Exception {
@@ -32,7 +36,7 @@ public class tpsi_server {
             File file = new File(pathToShow);
             if (file.isFile()) {
                 byte[] bytes  = new byte [(int)file.length()];
-                
+
                 exchange.getResponseHeaders().set("Content-Type", "application/txt");
                 exchange.sendResponseHeaders(200, file.length());
                 OutputStream os = exchange.getResponseBody();
@@ -42,7 +46,7 @@ public class tpsi_server {
             }
             else {
                 File[] listOfFiles = getListOfFiles(pathToShow);
-                String response = getHtml(listOfFiles);
+                String response = getHtml(listOfFiles, uri.toString());
 
                 exchange.getResponseHeaders().set("Content-Type", "text/html");
                 exchange.sendResponseHeaders(200, response.length());
@@ -56,14 +60,25 @@ public class tpsi_server {
         File folder = new File(path);
         return folder.listFiles();
     }
-    private static String getHtml(File[] files){
+    private static String getHtml(File[] files, String location){
         StringBuilder htmlBuilder = new StringBuilder();
 
         htmlBuilder.append("<html>");
         htmlBuilder.append("<head></head>");
         htmlBuilder.append("<body>");
+        htmlBuilder.append("<a href=\"http://localhost:8000");
+        htmlBuilder.append(location);
+        htmlBuilder.append("..\">");
+        htmlBuilder.append("..</a><br/>");
         for(File file : files){
+            htmlBuilder.append("<a href=\"http://localhost:8000");
+            //htmlBuilder.append(location.equals("/") ? "" : location);
+            //htmlBuilder.append("/");
+            htmlBuilder.append(location);
             htmlBuilder.append(file.getName());
+            htmlBuilder.append(file.isFile() ? "\">" : "/\">");
+            htmlBuilder.append(file.getName());
+            htmlBuilder.append("</a>");
             htmlBuilder.append("<br/>");
         }
         htmlBuilder.append("</body>");
